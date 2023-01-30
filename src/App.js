aec94fda645b3e62d30ba73bdb2706797bde0f9d
Kaja Królikowska <krolikowska.kaja@gmail.com>
2023-01-30 23:07:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>It will be a dictionary app</h1>
+      <button className="btn btn-primary">Test bootstrap button</button>
+    </div>
+  );
+}
+
+export default App;
