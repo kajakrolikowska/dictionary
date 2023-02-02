f7f32198936dc1c1a3c28a979a9f6f2a91a1829a
Kaja Królikowska <krolikowska.kaja@gmail.com>
2023-02-02 12:23:48 +0100

    Added footer with link too Github and Netlify.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <Container className="AppContainer">
-        <Dictionary />
+      <Container className="AppWrap">
+        <Container className="AppContainer">
+          <Dictionary />
+        </Container>
+        <footer>
+          This app was coded by Kaja Królikowska, it is open-sourced on{" "}
+          <a
+            href="https://github.com/kajakrolikowska/dictionary"
+            alt="GitHub repository link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://moonlit-gaufre-0b0dcb.netlify.app"
+            alt="Link to website"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
       </Container>
     </div>
   );
